34bb24fb55cf741a661012e241d44781d768a231
AbhishekBilagi <abhishekbilagi1998@gmail.com>
2022-04-13 17:56:47 +0530

    datatypes

--- ConsoleApp_1/Program.cs
+++ ConsoleApp_1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+//commite ctr+k+C
+//uncommite ctr+k+u
+
+namespace Test
+{
+    public class Demo
+    {
+        public static void Main()
+        {
+            int a = 25;
+            //string formatter
+            Console.WriteLine("Int value is: {0}",a);
+
+            float b = 45.25f;
+            double c = 56.25635;
+            Console.WriteLine("Float value is:{0} Double value is {1}", b,c);
+
+            string s = "Abhi";
+            //String Interpolation
+            Console.WriteLine( $"String value is {s}");
+
+            bool e=true;
+            char ch = 'A';
+            Console.WriteLine($"Boolean is:{e} char is:{ch}");
+        }
+    }
+}
